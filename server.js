57f604c2f95da8bfdc8193ae35d01b4762f2e6ee
Stino89 <ftr1989@gmail.com>
2024-03-10 11:04:53 -0700

    server update

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 require('dotenv').config()
+const userController = require('./controllers/users')
 
 const app = express()
 
+// routes
+app.use('/users', userController)
+
 const PORT = process.env.PORT || 8082
 
-app.listen(PORT, console.log(`listening on port ${PORT}`))
+app.listen(PORT, console.log(`listening on port ${PORT}`)) // http://localhost:8082
